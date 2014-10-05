2b6b5248b844ab08a781803a1d7abb62a5aab859
jgalinha <j.b.galinha@gmail.com>
2014-10-05 16:56:33 +0100

    aula3 - funcoescauda

--- aula3/aula3.py
+++ aula3/aula3.py
@@ -14,5 +14,22 @@ def frec(l1, x, n):
     else:
         return [x] + frec(l1, x, n-1)
 
-l2 = frec([], 'Bu ', 5)
+def factorial(n):
+    if n <= 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def factorial_cauda(n):
+    def fact(i=n, acc=1):
+        if i == 0:
+            return acc
+        else:
+            return fact(i-1, (acc * i))
+    return fact()
+
+print factorial_cauda(5)
+print factorial(5)
+
+l2 = frec([], 'Bu ', 2)
 print l2
