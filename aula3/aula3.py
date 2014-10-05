2105f3d1126225dcc120599942721de7b8b55bcd
José Eduardo Beiçudo Galinha <j.b.galinha@gmail.com>
2014-10-05 16:22:12 +0100

    aula3
    funcies2.py
    exemplo aula3

--- aula3/aula3.py
+++ aula3/aula3.py
@@ -3,3 +3,16 @@
 # data: 05 de outubro de 2014
 #
 
+def frec(l1, x, n):
+    '''
+    :param frec: devolve uma lista com n elementos x
+    :param x: o elemento a repetir
+    :param n: numero de repetições
+    '''
+    if not n:
+        return l1
+    else:
+        return [x] + frec(l1, x, n-1)
+
+l2 = frec([], 'Bu ', 5)
+print l2
